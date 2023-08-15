04aa0d497c94023a4c0cd4b0e77a708daaf9c3a8
TofaelKabir <tofaelkabir@gmail.com>
2023-08-14 20:34:48 -0400

    Update of Day01

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -10,21 +10,26 @@ import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class BaseClass {
-	public WebDriver driver;
-	public HomePage homePage;
+	public WebDriver driver; // or we can use protected type, otherwise it is default type
+	public HomePage homePage; // or we can use protected type, otherwise it is default type
 	
 	// Before start a test what need to do?
 	@BeforeMethod
 	public void setUp() {
 		// First job is to recognize the location of driver from your device
-		// right click on chromedriver.exe --- properties -- copy the location an dpaste below
+		// right click on chromedriver.exe --- properties -- copy the location an paste below
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tofael\\eclipse-workspace\\gov.cms.portal.may\\driver\\chromedriver.exe");
 		// We instantiated the driver here
 		driver = new ChromeDriver();
+		// maximize method maximize the window
 		driver.manage().window().maximize();
+		// deleteAllCookies do delete the cookies
 		driver.manage().deleteAllCookies();
+		// get method is used to get the targeted url
 		driver.get("https://portal.cms.gov/portal/");
+		// Pageloadtimeout is to wait to load the page for curtain amount of time
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		// Implicitly wait is used to wait for each web element
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		homePage = new HomePage(driver);
 	}
