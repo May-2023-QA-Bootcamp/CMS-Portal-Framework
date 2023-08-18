3395fe3aac9372965b9304af09480ac6e74b3683
TofaelKabir <tofaelkabir@gmail.com>
2023-08-17 21:40:44 -0400

    CMS Portal Framework Day03

--- src/test/java/pages/HomePageTest.java
+++ src/test/java/pages/HomePageTest.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import baseUtil.BaseClass;
@@ -54,9 +55,14 @@ public class HomePageTest extends BaseClass {
 		homePage.inputTextInUserIdField();
 	}
 	
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void inputTextInUserIdAndPasswordFieldThenClickLoginButtonTest () throws InterruptedException {
 		homePage.inputTextInUserIdAndPasswordFieldThenClickLoginButton();
+	}
+	
+	@Test(enabled = false)
+	public void clickUnlockTest() throws InterruptedException {
+		homePage.clickUnlock();
 	}
 	
 	
@@ -64,5 +70,134 @@ public class HomePageTest extends BaseClass {
 	public void clickLogoTest() throws InterruptedException {
 		homePage.clickLogo();
 	}
+	
+	// Important: Use of xpath as locator
+	// Web Element: New User Registration Button
+	// We can use a web element directly in the test class, that is also common, but different people like different way
+	@Test (enabled = false)
+	public void use_of_xpath_as_locator_in_new_user_registration_button() throws InterruptedException {
+		driver.findElement(By.xpath("//a[contains(text(), 'New')]")).click();
+		Thread.sleep(4000);
+	}
+	
+	// This is best scripted in ebay (I will show it later)
+	// Important: Use of linkText as locator
+	// Web Element: forgot User Id
+	@Test(enabled = false)
+	public void use_of_linkText_as_locator_in_forgot_user_id_hyperlink() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.findElement(By.linkText("User ID")).click();
+		Thread.sleep(5000);
+	}
+	
+	// Important: Use of partialLinkText as locator
+	// Web Element: forgot Password
+	@Test(enabled = false)
+	public void use_of_partialLinkText_as_locator_in_forgot_password_hyperlink() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.findElement(By.partialLinkText("Passwo")).click();
+		Thread.sleep(5000);
+	}
+	
+	// Error: 'Compound class names not permitted',
+	// so even if you see class value is unique but separated by space, avoid this as class name locator
+	// it will show error and failed
+	
+	@Test(enabled = false)
+	public void why_we_use_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.className("cms-icon cms-sprite-loggedout ms-3")).click();
+	}
+	
+	// Web Element: New User Registration Button
+	// Important: Use of cssSelector as locator
+	// cssSelector is very important for interview
+	// cssSelector by class --> inside the string, first put html tag, then dot, then write value of the class attribute 
+	@Test(enabled = false)
+	public void role1_use_of_cssSelector_as_locator_in_new_user_registration_button () {
+		driver.findElement(By.cssSelector("a.cms-newuser-reg")).click();
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// if you wanna create cssSelector by id 
+	// first html tag, then # (hash), then write the value of id attribute inside the string
+	@Test(enabled = false)
+	public void role2_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button#cms-login-submit")).click();
+	}
+	
+	// Web Element: logo
+	// Important: Use of cssSelector as locator
+	// if the class (compound class) contain separate words [cms-icon cms-sprite-loggedout ms-3], 
+	// they are actually different class, then
+	// we have to close the gap between classes by putting dot/period between them
+	// cssSelector by class --> htmltag.class name value 
+	// remove the space between words in class value, and replace with dot or period
+	@Test(enabled = false)
+	public void role3_use_of_cssSelector_as_locator_in_logo () {
+		driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).click();
+	}
+	
+	// Above 3 is mostly used 90% cases of css selector, also very common interview question
+	// below 3 high level, no need to see if you feel they are tough
+	
+	// Use of cssSelector as locator
+	// Web Element: login Button
+	// high level, you can ignore
+	// cssSelector by another attribute
+	// --> htmltag[any attribute = 'value of the attribute' ] , in xpath - you use // and @, which is absent here 
+	// and you can use any other attribute name except id and class
+	@Test(enabled = false)
+	public void role4_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: NUR Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag.value of class[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role5_use_of_cssSelector_as_locator_in_login_button() throws InterruptedException {
+		driver.findElement(By.cssSelector("a.cms-newuser-reg[title='New User Registration']")).click();
+		Thread.sleep(5000);
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag#value of ID[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = true)
+	public void role6_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button#cms-login-submit[name='Submit Login']")).click();
+	}
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
